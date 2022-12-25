1fadf7694f25bbcb266b47c0e03648901b477f70
davidmungai <mungahdaudi@gmail.com>
2022-12-25 22:30:05 +0300

    fix:added netlify.toml for deployment

--- svelte.config.js
+++ svelte.config.js
@@ -7,16 +7,7 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter({
-			// if true, will create a Netlify Edge Function rather
-			// than using standard Node-based functions
-			edge: false,
-
-			// if true, will split your app into multiple functions
-			// instead of creating a single one for the entire app.
-			// if `edge` is true, this option cannot be used
-			split: false
-		})
+		adapter: adapter()
 	}
 };
 
